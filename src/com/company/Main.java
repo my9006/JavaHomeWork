a267ebe605a405d6c45753663b5f18862c597355
Mkhitar Mkrtchyan <mkhitarmkrtchyan@workfront.com>
2020-05-31 23:43:24 +0400

    Late patch

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -14,68 +14,69 @@ public class Main {
 		Random random = new Random();
 		Scanner scanner = new Scanner(System.in);
 		//		5.1
-		//		twoNumberSum(random.nextInt(), random.nextInt());
+		twoNumberSum(random.nextInt(), random.nextInt());
 
 		//		5.2
-		//		System.out.println("Input 2 numbers to get their sum");
-		//		twoNumberSum(scanner.nextInt(), scanner.nextInt());
+		System.out.println("Input 2 numbers to get their sum");
+		twoNumberSum(scanner.nextInt(), scanner.nextInt());
 
 		//		5.3
-		//		System.out.println("Input two numbers to get the cube of their product");
-		//		calculateCubeOfProduct(scanner.nextInt(), scanner.nextInt());
+		System.out.println("Input two numbers to get the cube of their product");
+		calculateCubeOfProduct(scanner.nextInt(), scanner.nextInt());
 
 		//		5.4
-		//		System.out.println("Input three ints to get the smallest of them");
-		//		smallestOfThreeInts(scanner.nextInt(), scanner.nextInt(), scanner.nextInt());
+		System.out.println("Input three ints to get the smallest of them");
+		smallestOfThreeInts(scanner.nextInt(), scanner.nextInt(), scanner.nextInt());
 
 		//		5.5
-		//		System.out.println("Input a string to get the middle character");
-		//		printMiddleCharacterOfString(scanner.next());
+		System.out.println("Input a string to get the middle character");
+		printMiddleCharacterOfString(scanner.next());
 
 		//		5.6
-		//		System.out.println("Input a sentence to get the number of Words");
-		//		printCountOfWords(scanner.nextLine());
+		System.out.println("Input a sentence to get the number of Words");
+		printCountOfWords(scanner.nextLine());
 
 		//		5.7
-		//		System.out.println("Input the year to check whether it's leap or not");
-		//		isTheYearLeap(scanner.nextInt());
+		System.out.println("Input the year to check whether it's leap or not");
+		isTheYearLeap(scanner.nextInt());
 
 		//		5.8
-		//		System.out.println("Input a string to check its validity as a password * Should cojntain at least 2 digits, be more or equal to 10 and contain only letters and numbers");
-		//		isPasswordValid(scanner.nextLine());
+		System.out.println("Input a string to check its validity as a password * Should cojntain at least 2 digits, be more or equal to 10 and contain only letters and numbers");
+		isPasswordValid(scanner.nextLine());
 
 		//		5.9
-		//		int radius = scanner.nextInt();
-		//		System.out.printf("The Area of %d radius circle is %f, and the circumference is %f", radius, calculateAreaOfCircle(radius), calculateCircumference(radius));
-		//		5.10
-		//		System.out.println("Input three sides of triangle");
-		//		int side1 = scanner.nextInt();
-		//		int side2 = scanner.nextInt();
-		//		int side3 = scanner.nextInt();
-		//		if(isTriangleValid(side1, side2, side3)){
-		//			System.out.printf("Perimeter is %d, and the area is %f", calculatePerimeterOfTriangle(side1, side2, side3), calculateAreaOfTriangle(side1, side2, side3));
-		//		}
-		//		5.11
-		//		arrayPrinter(intArrayGenerator(random));
-		//		5.12
-		//		ascendingSortIntArray(intArrayGenerator(random));
-		//		5.13
-		//		descendingSortIntArray(intArrayGenerator(random));
-		// 		5.14
-		//		primePrinter(100);
-		//		5.15
-		//		arrayPrinter(reverseArray(intArrayGenerator(random)));
-		//		5.16
-		//		System.out.println(isStringPalindrome("debed")?"Palindrome":"Not palindrome");
+		int radius = scanner.nextInt();
+		System.out.printf("The Area of %d radius circle is %f, and the circumference is %f", radius, calculateAreaOfCircle(radius), calculateCircumference(radius));
+//		5.10
+		System.out.println("Input three sides of triangle");
+		int side1 = scanner.nextInt();
+		int side2 = scanner.nextInt();
+		int side3 = scanner.nextInt();
+		if (isTriangleValid(side1, side2, side3)) {
+			System.out.printf("Perimeter is %d, and the area is %f", calculatePerimeterOfTriangle(side1, side2, side3), calculateAreaOfTriangle(side1, side2, side3));
+		}
+		//						5.11
+		int[] ints = new int[10];
+		arrayPrinter(intArrayGenerator(ints));
+		//				5.12
+		arrayPrinter(ascendingSortIntArray(intArrayGenerator(ints)));
+		//				5.13
+		arrayPrinter(descendingSortIntArray(intArrayGenerator(ints)));
+		//		 		5.14
+		primePrinter(100);
+		//				5.15
+		arrayPrinter(reverseArray(intArrayGenerator(ints)));
+		//				5.16
+		System.out.println(isStringPalindrome("debed") ? "Palindrome" : "Not palindrome");
 		//		5.17
-		//		System.out.println(factorialCalculator(14));
+		System.out.println(factorialCalculator(14));
 		//		5.18
-		//		stringMatrixPrinter(initializeRandomMatrix(10));
+		stringMatrixPrinter(initializeRandomMatrix(10));
 		//		5.19
-		//		int [][] matrix = intMatrixGenerator(scanner.nextInt());
-		//		rotateMatrixCounterClockWise(matrix);
-		//		rotateMatrixClockWise(matrix);
-		//		turnMatrixUpsideDown(matrix);
+		int[][] matrix = intMatrixGenerator(scanner.nextInt());
+		rotateMatrixCounterClockWise(matrix);
+		rotateMatrixClockWise(matrix);
+		turnMatrixUpsideDown(matrix);
 		helloMenu();
 
 	}
@@ -136,12 +137,12 @@ public class Main {
 		return Math.sqrt(s * (s - side1) * (s - side2) * (s - side3));
 	}
 
-	public static int[] intArrayGenerator(Random random) {
-		int[] ints = new int[10];
+	public static int[] intArrayGenerator(int[] array) {
+		Random random = new Random();
 		for (int i = 0; i < 10; i++) {
-			ints[i] = random.nextInt();
+			array[i] = random.nextInt();
 		}
-		return ints;
+		return array;
 	}
 
 	public static void arrayPrinter(int[] array) {
@@ -150,17 +151,19 @@ public class Main {
 		}
 	}
 
-	public static void ascendingSortIntArray(int[] array) {
+	public static int[] ascendingSortIntArray(int[] array) {
 		Arrays.sort(array);
+		return array;
 	}
 
-	public static void descendingSortIntArray(int[] array) {
+	public static int[] descendingSortIntArray(int[] array) {
 		ascendingSortIntArray(array);
 		for (int i = 0; i < array.length / 2; i++) {
 			int temp = array[i];
 			array[i] = array[array.length - i - 1];
 			array[array.length - i - 1] = temp;
 		}
+		return array;
 	}
 
 	public static void primePrinter(int till) {
@@ -319,9 +322,9 @@ public class Main {
 	}
 
 	public static void matrixPrinter(int[][] matrix) {
-		for (int[] array: matrix){
-			for(int element:array){
-				System.out.print(element+", ");
+		for (int[] array : matrix) {
+			for (int element : array) {
+				System.out.print(element + ", ");
 			}
 			System.out.println();
 		}
