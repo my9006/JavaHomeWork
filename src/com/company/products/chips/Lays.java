5144fe3950836bf9cbdb4a92108700bddd47cb9e
Mkhitar Mkrtchyan <mkhitarmkrtchyan@workfront.com>
2020-07-01 09:38:10 +0400

    Vending machine

---
+++ src/com/company/products/chips/Lays.java
@@ -0,0 +1,11 @@
+package com.company.products.chips;
+
+import com.company.products.common.Chips;
+import com.company.products.common.ChipsFlavour;
+
+public class Lays extends Chips {
+
+	public Lays(int price, ChipsFlavour flavour) {
+		super(price, flavour);
+	}
+}
